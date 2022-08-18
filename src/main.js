352ef739233c8c4c3e926420384e7d7f73c93383
sem_wett <90400355+semwett0301@users.noreply.github.com>
2022-08-18 20:38:54 +0300

    The PrimeVue was added

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from "@/store"
 import router from "@/router/router";
+import PrimeVue from 'primevue/config';
+
 
 createApp(App)
     .use(store)
     .use(router)
+    .use(PrimeVue)
     .mount('#app')
